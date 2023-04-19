4f830db46deaf8379bf4957362f55cf7ec97d9a9
F1p <thomsonpa@f1p.co.uk>
2023-04-19 21:38:10 +0100

    Update MQTTConfig.h

--- MQTTConfig.h
+++ MQTTConfig.h
@@ -1,9 +1,9 @@
-#define MQTT_SERVER Set_Your_MQTT_Server_Here //  Fill in below, and delete this line
-//#define MQTT_SERVER "192.168.1.1"
+//#define MQTT_SERVER Set_Your_MQTT_Server_Here //  Fill in below, and delete this line
+#define MQTT_SERVER "192.168.1.1"
 #define MQTT_PORT 1883
 
 
-#define MQTT_BASETOPIC "Ecodan/HomeHeatPump"
+#define MQTT_BASETOPIC "Ecodan/ASHP"
 
 #define MQTT_STATUS MQTT_BASETOPIC "/Status"
 #define MQTT_COMMAND MQTT_BASETOPIC "/Command"
@@ -23,14 +23,19 @@
 #define MQTT_COMMAND_ZONE1_FLOW_SETPOINT MQTT_COMMAND_ZONE1  "/FlowSetpoint"
 #define MQTT_COMMAND_ZONE1_CURVE_SETPOINT MQTT_COMMAND_ZONE1  "/CurveSetpoint"
 
+#define MQTT_COMMAND_ZONE2_TEMP_SETPOINT MQTT_COMMAND_ZONE2  "/TempSetpoint"
+#define MQTT_COMMAND_ZONE2_FLOW_SETPOINT MQTT_COMMAND_ZONE2  "/FlowSetpoint"
+#define MQTT_COMMAND_ZONE2_CURVE_SETPOINT MQTT_COMMAND_ZONE2  "/CurveSetpoint"
+
 #define MQTT_COMMAND_SYSTEM_HEATINGMODE MQTT_COMMAND_SYSTEM "/HeatingMode"
 #define MQTT_COMMAND_HOTWATER_SETPOINT MQTT_COMMAND_HOTWATER "/Setpoint"
+#define MQTT_COMMAND_HOTWATER_BOOST MQTT_COMMAND_HOTWATER "/Boost"
 #define MQTT_COMMAND_SYSTEM_POWER MQTT_COMMAND_SYSTEM "/Power"
 #define MQTT_COMMAND_SYSTEM_TEMP MQTT_COMMAND_SYSTEM "/Temp"
 
 
 #define MQTT_LWT MQTT_BASETOPIC "/LWT"
 
-#define MQTT_USER  Set_Your_MQTT_User_Name_Here // Fill in below and delete this line
-//#define MQTT_USER "USER"
+//#define MQTT_USER  Set_Your_MQTT_User_Name_Here // Fill in below and delete this line
+#define MQTT_USER "USER"
 #define MQTT_PASS "PASS"
